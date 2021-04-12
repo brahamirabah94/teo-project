f2b14927ae07305ca3cb20e358a4ce5001d2bbef
brahamirabah94 <81314758+brahamirabah94@users.noreply.github.com>
2021-04-12 14:51:58 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,9 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name 
   dns_prefix          = var.cluster_name
 
   default_node_pool {
