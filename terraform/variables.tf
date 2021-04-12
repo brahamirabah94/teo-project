3c55f4626b0da124d0b9c973e7da0cb214758c57
brahamirabah94 <81314758+brahamirabah94@users.noreply.github.com>
2021-04-12 14:13:24 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,24 @@ variable "location" {
   type        = string
   description = "Resources location in Azure"
 }
+variable "storage-account-name" {
+  type        = string
+  description = "strorage account name"
+}
 
+variable "backend-rg" {
+  type        = string
+  description = "backend RG in Azure"
+}
+variable "container-name" {
+  type        = string
+  description = "container name"
+}
+
+variable "backend-name" {
+  type        = string
+  description = "backend name key in Azure"
+}
 variable "cluster_name" {
   type        = string
   description = "AKS name in Azure"
