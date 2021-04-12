a9308a54998f99859b28fc05159ade3f8e8e5db7
brahamirabah94 <81314758+brahamirabah94@users.noreply.github.com>
2021-04-12 14:08:16 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
-}
-# donner droit a notre cluster de pull des images
-# resource "azurerm_role_assignment" "role_acrpull" {
-#   scope                            = azurerm_container_registry.acr.id
-#   role_definition_name             = "AcrPull"
-#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-#   skip_service_principal_aad_check = true
-# }
-#
-# resource "azurerm_container_registry" "acr" {
-#   name                = var.acr_name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = var.location
-#   sku                 = "Basic"
-#   admin_enabled       = false
-# }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
@@ -33,22 +17,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
     availability_zones  = [1, 2, 3]
     enable_auto_scaling = false
   }
-/*
-  service_principal  {
-    client_id = var.serviceprinciple_id
-    client_secret = var.serviceprinciple_key
-  }
-*/
+
   identity {
     type = "SystemAssigned"
   }
-  # for flux configuration
   network_profile {
     network_plugin = "azure"
     network_policy = "calico"
   }
-  # network_profile {
-  #   load_balancer_sku = "Standard"
-  #   network_plugin    = "kubenet"
-  # }
+
 }
