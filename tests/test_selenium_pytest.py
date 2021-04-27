8097d099d3b4c411102f23e16185630f03c0288b
brahamirabah94 <81314758+brahamirabah94@users.noreply.github.com>
2021-04-27 15:39:22 +0200

    Update test_selenium_pytest.py

--- tests/test_selenium_pytest.py
+++ tests/test_selenium_pytest.py
@@ -1,40 +1,42 @@
 from selenium import webdriver
 import pytest
 
-class TestSample():
-    @pytest.fixture
-    def test_setup():
-        global driver
-        driver = webdriver.Chrome()
-        driver.implicitly_wait(10)
-        driver.maximize_window()
-        driver.get("https://8000-cs-453440873740-default.cs-europe-west1-akef.cloudshell.dev/index.html")
-        yield 
-        driver.close()
-        driver.quit()
-        print("fin des tests")
 
-    def test_home_page_title(test_setup):
-        x = driver.title
+@pytest.fixture
+def test_setup():
+    global driver
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    driver.get("https://8000-cs-453440873740-default.cs-europe-west1-akef.cloudshell.dev/index.html")
+    yield 
+    driver.close()
+    driver.quit()
+    print("fin des tests")
+
+class TestSample(test_setup):
+
+    def test_home_page_title(self):
+        x = self.driver.title
         assert x == "Projet Teoschool Brahami Rabah"
 
-    def test_git_page_acces():
-        driver.find_element_by_id("github-image").click()
-        githubTitle = driver.title
+    def test_git_page_acces(self):
+        self.driver.find_element_by_id("github-image").click()
+        githubTitle = self.driver.title
         assert "brahamirabah94" in githubTitle
 
-    def test_dockercoin_page_acces():
-        driver.find_element_by_id("dockercoin-image").click()
-        CoinTitle = driver.title
+    def test_dockercoin_page_acces(self):
+        self.driver.find_element_by_id("dockercoin-image").click()
+        CoinTitle = self.driver.title
         assert "Dockercoin" in CoinTitle 
 
-    def test_teolia_page_acces():
-        driver.find_element_by_id("teo-image").click()
-        TeoliaTitle = driver.title
+    def test_teolia_page_acces(self):
+        self.driver.find_element_by_id("teo-image").click()
+        TeoliaTitle = self.driver.title
         assert "Teolia" in TeoliaTitle 
 
 
-    def test_linkedin_page_acces():
-        driver.find_element_by_id("linkedin-image").click()
-        LinkedinTitle = driver.title
+    def test_linkedin_page_acces(self):
+        self.driver.find_element_by_id("linkedin-image").click()
+        LinkedinTitle = self.driver.title
         assert "Rabah" in linkedinTitle 
